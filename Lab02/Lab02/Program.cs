6f5d36d9366d8db0f2f2d200e00beab66c0fe927
s26839 <s26839@pjwstk.edu.pl>
2024-03-08 17:05:07 +0100

    added methods

--- Lab02/Lab02/Program.cs
+++ Lab02/Lab02/Program.cs
@@ -5,3 +5,14 @@ Console.WriteLine("MOD 1");
 Console.WriteLine("MOD 2.1");
 Console.WriteLine("MOD 3");
 Console.WriteLine("MOD 4");
+
+static double GetAvg(int[] aa)
+{
+    double avg = 0;
+    for (int i = 0; i < aa.Length; i++)
+    {
+        avg += aa[i];
+    }
+
+    return avg / aa.Length;
+}

33fc25a61d237cc7343d896a00822a9e5f98e411
jakubmordal <s26839@pjwstk.edu.pl>
2024-03-14 23:36:10 +0100

    task4

---
+++ Lab02/task4/Program.cs
@@ -0,0 +1,43 @@
+﻿public class Program2
+{
+    public static void Main(string[] Args)
+    {
+        Console.WriteLine("Hello, World!");
+        Console.WriteLine(Avg(new[] { 1, 2, 3, 4 }));
+        Console.WriteLine(Max(new[] { 1,5,6,2, 3, 4 }));
+    }
+   
+    public static double Avg(int[] a)
+
+    {
+        double sum = 0;
+        for (int i = 0; i < a.Length; i++)
+        {
+            sum += a[i];
+        }
+
+        double avg = sum / a.Length;
+        return avg;
+    }
+
+
+    public static int Max(int[] a)
+    { 
+        int max = a[0];
+        for (int i = 0; i < a.Length; i++)
+        {
+           
+            int temp = a[i];
+          
+            if (max<=temp)
+            {
+                max = a[i];
+               
+
+            }
+           
+        }
+
+        return max;
+    }
+}
